9a8c28e0de00508152d42254a2bef428c9539f27
BessemFer <bassem.fer@gmail.com>
2016-12-18 02:38:18 +0100

    application terminer

---
+++ README.md
@@ -0,0 +1,5 @@
+# simple to do list (Node.jd express.js)
+ Application simple pour montrer d'une facon simple:
+	 configuration de l'application
+	 Utilisation des middleware
+	 Definition des routes 
--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 var express = require('express');
 var session = require('cookie-session'); // charger  middleware de session
 var bodyParser = require('body-parser'); // charger le middleware de gestion des paramétres 
-var urlencodedParser = bodyParser.urlencoded({extanded:false});
+var path  = require('path');
+var urlencodedParser = bodyParser.urlencoded({extended:false});
 
 var app = express();
 /*
 config app
 */
 app.set('view engine', 'ejs');
+app.set('views',path.join( __dirname, 'views'));
+//app.set('/css',path.join( __dirname, 'css'));
 
 /*
 use middleware
 */
 // utilisation des sessions
 app.use(session({secret: 'simplesecret'}));
+app.use(express.static(path.join(__dirname, '/assest')));
 
 /*
 define routes 
@@ -40,3 +44,16 @@ app.post('/todo/ajouter',urlencodedParser, function(req, res, next){ // importan
 	res.redirect('/todo');
 });
 // suppression d'un element de la todo list
+app.get('/todo/supprimer/:id', function(req, res) {
+	if(req.params.id !=='') {
+		req.session.todolist.splice(req.params.id, 1);
+	}
+	res.redirect('/todo');
+});
+
+// rederiction ver une page todo si la page demander n'est pas reconue 
+app.use(function(req, res, next) {
+	res.redirect('/todo');
+});
+
+app.listen(8000);
